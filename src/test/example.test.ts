ec4c8b9537cddfb4ca91c6793abd1a3482b113e6
Alexis Cesar <alexis.almeida@programmers.com.br>
2022-07-20 14:25:02 -0300

    Add test script and test example

---
+++ src/test/example.test.ts
@@ -0,0 +1,11 @@
+describe('collection or component name', () => {
+
+    it('should sum', () => {
+        let expected = 2;
+        expect(1 + 1).toBe(expected);
+    });
+
+    it.todo('todo test');
+    it.todo('🥑');
+
+});
