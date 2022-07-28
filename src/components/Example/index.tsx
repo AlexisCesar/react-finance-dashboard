cb974aeb66385b66dd37623a2911c66604dffd11
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-07-28 11:40:00 -0300

    Add global style and component structure
    - Changed global css location
    - Added global color variables
    - Changed font
    - Created example component
    - Created Home page
    - Added indexes

---
+++ src/components/Example/index.tsx
@@ -0,0 +1,9 @@
+import style from './example.module.scss';
+
+export const Example = () => {
+  return (
+    <>
+      <h1 className={style.title}>Example</h1>
+    </>
+  );
+};
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import style from './Home.module.scss';
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
