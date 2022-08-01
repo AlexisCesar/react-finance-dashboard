07476a83fc86847ca38f0cba150670d75d4a6a28
Rafael Moretti Silva <rafamoresi@gmail.com>
2022-08-01 14:17:22 -0300

    Add icons

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,24 +1,34 @@
 import style from './modal.module.scss';
 
 export const Modal = () => {
-    return (
-        <>
-            <div className={style["modal-wrapper"]}>
-                <div className={style["modal-box"]}>
-                    <span>x</span>
-                    <h2>Create Transaction</h2>
-                    <input type="text" placeholder="Name" />
-                    <input type="number" placeholder="Price" />
+  return (
+    <>
+      <div className={style["modal-wrapper"]}>
+        <div className={style["modal-box"]}>
+          <span>
+            <img src="src\assets\images\close-button.png" />
+          </span>
+          <h2>Create Transaction</h2>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="number" placeholder="Price" min={0} />
 
-                    <div className={style["transaction-type"]}>
-                        <button>Income</button>
-                        <button>Outcome</button>
-                    </div>
-
-                    <input type="text" placeholder="Category" />
-                    <button>Create</button>
-                </div>
+            <div className={style["transaction-type"]}>
+              <button className={style["income"]}>
+                <img src="src\assets\images\income.png" />
+                Income
+              </button>
+              <button className={style["outcome"]}>
+                <img src="src\assets\images\outcome.png" />
+                Outcome
+              </button>
             </div>
-        </>
-    );
+
+            <input type="text" placeholder="Category" />
+            <button>Create</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
