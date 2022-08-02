6a6d0e1dec1a0f2710b42f77a86ec95ffb904c6b
Pilar Abreu <pilar.abreu@programmers.com.br>
2022-08-02 18:49:32 -0300

    header added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Card } from '../../components';
 import style from './Home.module.scss';
+import { Header } from '../../components/header';
 
 export const Home = () => {
   return (
     <div className={style['container']}>
       <Card />
+      <Header />
     </div>
   );
 };
