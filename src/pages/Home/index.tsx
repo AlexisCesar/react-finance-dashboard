fa1a8837049935f509389b1e3eb2eaacdbbb687f
pilar beatriz <68299526+pilarhaddad@users.noreply.github.com>
2022-08-08 10:49:44 -0300

    Create header
    * header added
    
    * align elements
    
    * Add cards and align
    
    Co-authored-by: Pilar Abreu <pilar.abreu@programmers.com.br>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import { Card } from '../../components';
 import style from './Home.module.scss';
+import { Header } from '../../components/header';
 
 export const Home = () => {
   return (
-    <div className={style['container']}>
-      <Card />
+    <div>
+      <Header />
+      <div className={style['container']}>
+        <Card />
+      </div>
     </div>
   );
 };
